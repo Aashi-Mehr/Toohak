9e05f34e06bc77a4496a06275933ce6e195757ce
Alya Nur Najwa Azmin Shah <z5445823@ad.unsw.edu.au>
2023-09-21 19:08:46 +1000

    implemented function adminAuthRegister

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,7 @@
+// Function : adminAuthRegister
+// Input : email, password, nameFirst, nameLast
+// Output : authUserId
+
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return { 
         authUserId: 1,
