3f46a662fec89b115dc6020bf73c3e9c59e00e40
Aashi Mehrotra <z5479429@ad.unsw.edu.au>
2023-09-21 19:23:45 +1000

    Added a stub for the adminQuizRemove function

---
+++ src/quiz.js
@@ -0,0 +1,13 @@
+/*  adminQuizRemove
+    Given a particular quiz, permanently remove the quiz.
+
+    Parameters:
+        authUserId:
+        quizId:
+    
+    Output:
+        { }: Empty List
+ */
+function adminQuizRemove( authUserId, quizId ) {
+    return { };
+}
