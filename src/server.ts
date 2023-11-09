953e05f0ac568fa99dbaaf83a0cf88f97607194d
Aashi Mehrotra <z5479429@ad.unsw.edu.au>
2023-11-09 19:47:14 +1100

    Fixing conflicts

--- src/server.ts
+++ src/server.ts
@@ -129,98 +129,6 @@ app.put('/v1/admin/user/password', (req: Request, res: Response) => {
 });
 
 // ====================================================================
-<<<<<<< HEAD
-=======
-//  ========================= QUIZ FUNCTIONS =========================
-// ====================================================================
-//  ========================== ITERATION 2 ===========================
-// ====================================================================
-
-/* // adminQuizList
-app.get('/v1/admin/quiz/list', (req: Request, res: Response) => {
-  const token = parseInt(req.query.token as string);
-  const response = adminQuizList(token);
-
-  if ('error' in response) return res.status(401).json(response);
-  res.json(response);
-});
-
-// adminQuizInfo
-app.get('/v1/admin/quiz/:quizid', (req: Request, res: Response) => {
-  const quizId = parseInt(req.params.quizid);
-  const token = parseInt(req.query.token as string);
-  const response = adminQuizInfo(token, quizId);
-
-  if ('error' in response) {
-    if (response.error === token401) return res.status(401).json(response);
-    return res.status(403).json(response);
-  }
-  res.json(response);
-});
-
-// adminQuizTrash
-app.get('/v1/admin/quiz/trash', (req: Request, res: Response) => {
-  const token = parseInt(req.query.token as string);
-  const response = adminQuizTrash(token);
-
-  if ('error' in response) return res.status(401).json(response);
-  res.json(response);
-});
-*/
-
-// ====================================================================
-//  ========================= QUESTION FUNCTIONS =====================
-// ====================================================================
-//  ========================== ITERATION 2 ===========================
-// ====================================================================
-
-/* // adminQuestionMove
-app.put('/v1/admin/quiz/:quizid/question/:questionid/move',
-  (req: Request, res: Response) => {
-    let { token, newPosition } = req.body;
-    const quesId = parseInt(req.params.questionid);
-    const quizId = parseInt(req.params.quizid);
-
-    token = parseInt(token);
-    newPosition = parseInt(newPosition);
-
-    const response = adminQuestionMove(token, newPosition, quesId, quizId);
-
-    if ('error' in response) {
-      if (response.error === token401) return res.status(401).json(response);
-      else if (response.error === unauth403) return res.status(403).json(response);
-      else return res.status(400).json(response);
-    }
-
-    res.json(response);
-    backupData();
-  });
-
-// adminQuestionDuplicate
-app.post('/v1/admin/quiz/:quizid/question/:questionid/duplicate',
-  (req: Request, res: Response) => {
-    const { token } = req.body;
-    const quiz = req.params.quizid;
-    const question = req.params.questionid;
-
-    const token1 = parseInt(token);
-    const quesId = parseInt(question);
-    const quizId = parseInt(quiz);
-
-    const response = adminQuestionDuplicate(token1, quesId, quizId);
-
-    if ('error' in response) {
-      if (response.error === token401) return res.status(401).json(response);
-      else if (response.error === unauth403) return res.status(403).json(response);
-      else return res.status(400).json(response);
-    }
-
-    res.json(response);
-    backupData();
-  }); */
-
-// ====================================================================
->>>>>>> origin/master
 //  ========================= AUTH FUNCTIONS =========================
 // ====================================================================
 //  =========================== VERSION 2 ============================
@@ -292,13 +200,8 @@ app.post('/v1/admin/quiz', (req: Request, res: Response) => {
 });
 
 // adminQuizTrash
-<<<<<<< HEAD
 app.get('/v1/admin/quiz/trash', (req: Request, res: Response) => {
   const token = parseInt(req.query.token as string);
-=======
-app.get('/v2/admin/quiz/trash', (req: Request, res: Response) => {
-  const token = parseInt(req.headers.token as string);
->>>>>>> origin/master
   res.json(adminQuizTrash(token));
 });
 
